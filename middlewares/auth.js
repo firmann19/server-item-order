3751541ad8aa73c510df3bc5e8dcc72dfdc4e1ec
Firman Ramadhan <firman19ramadhan@gmail.com>
2024-06-20 17:35:08 +0700

    push

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,39 +1,46 @@
-const UnauthorizedError = require("../errors/unauthorized");
-const { isTokenValid } = require("../utils/jwt");
+const jwt = require("jsonwebtoken");
+const { UnauthorizedError } = require("../errors/unauthorized");
 
 const authenticateUser = async (req, res, next) => {
   try {
     let token;
 
-    //check header
+    // Check header
     const authHeader = req.headers.authorization;
 
     if (authHeader && authHeader.startsWith("Bearer")) {
       token = authHeader.split(" ")[1];
     } else {
-      return res.status(401).send({
+      return res.status(401).json({
         status: false,
-        message: "Authenticated Invalid",
+        message: "Authentication Invalid",
         data: null,
-      })
+      });
     }
 
-    const payload = isTokenValid({ token });
-
-    // Attach the user and his permissions to the req object
-    req.user = {
-      id: payload.userId,
-      name: payload.name,
-      email: payload.email,
-      role: payload.role,
-    };
-
-    next();
+    // Verify JWT token
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({
+          status: false,
+          message: "Token is not valid",
+          data: null,
+        });
+      } else {
+        // Token is valid, attach decoded user payload to req.user
+        req.user = {
+          id: decoded.userId,
+          name: decoded.name,
+          email: decoded.email,
+          role: decoded.role,
+        };
+        next();
+      }
+    });
   } catch (error) {
     next(error);
   }
 };
-
 
 const authorizeRoles = (...rolesUser) => {
   return (req, res, next) => {
